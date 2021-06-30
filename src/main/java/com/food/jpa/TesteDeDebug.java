13aa0d3484c4cf01875635182de45f5eba512504
Diego Simon Carmona <diegosimoncarmona@gmail.com>
2021-06-29 23:16:02 -0300

    Classe de teste de debug

---
+++ src/main/java/com/food/jpa/TesteDeDebug.java
@@ -0,0 +1,34 @@
+package com.food.jpa;
+
+public class TesteDeDebug {
+
+    public static void main(String[] args) {
+        System.out.println("Teste mano");
+
+        System.out.println("Teste mano");
+
+        soTeste();
+
+        System.out.println("Teste mano");
+
+        soTeste2();
+
+        System.out.println("Teste mano");
+
+        soTeste3();
+
+        System.out.println("Teste mano");
+    }
+
+    public static void soTeste(){
+        System.out.println("Teste");
+    }
+
+    public static void soTeste2(){
+        System.out.println("Teste");
+    }
+
+    public static void soTeste3(){
+        System.out.println("Teste");
+    }
+}
