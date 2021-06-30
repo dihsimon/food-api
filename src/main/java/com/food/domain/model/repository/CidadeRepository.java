a826716546c62253967c6016b87121222dc80e7a
Diego Simon Carmona <diegosimoncarmona@gmail.com>
2021-06-29 23:04:43 -0300

    Incluindo Commentario.

--- src/main/java/com/food/domain/model/repository/CidadeRepository.java
+++ src/main/java/com/food/domain/model/repository/CidadeRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.food.domain.model.Cidade;
 
+/**
+ * Repositorio de Cidade
+ */
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 	
